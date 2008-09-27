d7ae8c45264d25b7e535f06b832983e1b1dc5cea
leviticus <leviticus@9c5736bd-fc4d-0410-b4fd-d14b4baf291c>
2008-09-27 08:06:46 +0000

    20080927

---
+++ tags/0.9.20080927/src/org/esxx/js/JSRequest.java
@@ -0,0 +1,407 @@
+/*
+     ESXX - The friendly ECMAscript/XML Application Server
+     Copyright (C) 2007-2008 Martin Blom <martin@blom.org>
+
+     This program is free software: you can redistribute it and/or
+     modify it under the terms of the GNU General Public License
+     as published by the Free Software Foundation, either version 3
+     of the License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.esxx.js;
+
+import org.esxx.*;
+import org.esxx.util.IO;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.HashMap;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.MimeHeaders;
+import javax.xml.soap.SOAPConstants;
+import javax.xml.soap.SOAPException;
+import org.mozilla.javascript.*;
+
+
+public class JSRequest
+  extends ScriptableObject {
+    public JSRequest() {
+      super();
+    }
+
+    public JSRequest(Request request, Context cx, Scriptable scope) {
+      this();
+
+      ESXX esxx    = ESXX.getInstance();
+
+      env     = cx.newObject(scope);
+      headers = cx.newObject(scope);
+      cookies = cx.newObject(scope);
+      accept  = cx.newObject(scope);
+      query   = cx.newObject(scope);
+      args    = null;
+
+      mimeHeaders = new MimeHeaders();
+      acceptValueOf = new FunctionObject("valueOf", acceptValueOfMethod, accept);
+
+      for (String name :  request.getProperties().stringPropertyNames()) {
+	String value = request.getProperties().getProperty(name).trim();
+
+	// Add environtment variable to esxx.env
+	ScriptableObject.putProperty(env, name, value);
+
+	// If this is an HTTP header, get the original name back
+	String hdr = esxx.cgiToHTTP(name);
+
+	if (hdr != null) {
+	  // Add real HTTP header to mimeHeaders and this.headers
+	  addHeader(hdr, value);
+
+	  // Decode cookies
+	  handleCookieHeader(hdr, value);
+
+	  // Decode Accept* HTTP headers
+	  handleAcceptHeader(hdr, value, cx, accept);
+
+	  // Decode Content-* HTTP headers
+	  handleContentHeader(hdr, value);
+
+	  // Handle SOAPAction
+	  if (hdr.equals("SOAPAction")) {
+	    soapAction = value;
+	  }
+	}
+
+	if (name.equals("QUERY_STRING")) {
+	  handleQueryHeader(value);
+	}
+      }
+
+      logger  = (JSLogger) cx.newObject(scope, "Logger", new Object[] { 
+	  request, request.getProperties().getProperty("SCRIPT_NAME") });
+
+      // Now parse the POST/PUT/etc. message
+      parseMessage(request, cx, scope);
+    }
+
+    public void setArgs(Scriptable uri_params) {
+      args = uri_params;
+    }
+
+    static public Object jsConstructor(Context cx,
+				       java.lang.Object[] args,
+				       Function ctorObj,
+				       boolean inNewExpr) {
+      return new JSRequest((Request) args[0], cx, ctorObj);
+    }
+
+
+    @Override
+    public String getClassName() {
+      return "Request";
+    }
+
+    public Scriptable jsGet_env() {
+      return env;
+    }
+
+    public Scriptable jsGet_headers() {
+      return headers;
+    }
+
+    public Scriptable jsGet_cookies() {
+      return cookies;
+    }
+
+    public Scriptable jsGet_accept() {
+      return accept;
+    }
+
+
+    public Scriptable jsGet_query() {
+      return query;
+    }
+
+    public Scriptable jsGet_args() {
+      return args;
+    }
+
+
+    public Scriptable jsGet_log() {
+      return logger;
+    }
+
+
+    public Object jsGet_message() {
+      return message;
+    }
+
+
+    public String jsGet_soapAction() {
+      return soapAction;
+    }
+
+
+    private Scriptable env;
+    private Scriptable headers;
+
+    private Scriptable cookies;
+    private Scriptable accept;
+    private Scriptable query;
+    private Scriptable args;
+
+    private Scriptable logger;
+
+    private Object message;
+
+    private String soapAction;
+    private String contentType;
+    private long contentLength;
+    private MimeHeaders mimeHeaders;
+
+    private Scriptable acceptValueOf;
+    static private java.lang.reflect.Method acceptValueOfMethod;
+    @SuppressWarnings("unused")
+	private static Object acceptValueOf(Context cx, Scriptable thisObj,
+					Object[] args, Function funObj) {
+      return thisObj.get("value", thisObj);
+    }
+    static {
+      try {
+	acceptValueOfMethod = JSRequest.class.getDeclaredMethod("acceptValueOf",
+								Context.class,
+								Scriptable.class,
+								Object[].class,
+								Function.class);
+      }
+      catch (NoSuchMethodException ex) {
+	throw new ESXXException("Failed to find JSRequest.acceptValueOf(): ", ex);
+      }
+    }
+
+
+    private void addHeader(String name, String value) {
+      mimeHeaders.addHeader(name, value);
+
+      ScriptableObject.putProperty(headers, name, value);
+    }
+
+
+    private void handleCookieHeader(String name, String value) {
+      // TODO
+    }
+
+
+    private void handleAcceptHeader(String hdr, String value, Context cx, Scriptable accept) {
+      String subname;
+
+      if (hdr.equals("Accept")) {
+	subname = "media";
+      }
+      else if (hdr.startsWith("Accept-")) {
+	subname = hdr.substring(7).toLowerCase();
+      }
+      else {
+	// Do nothing
+	return;
+      }
+
+      Map<Double, List<Scriptable>> objects = new TreeMap<Double, List<Scriptable>>();
+
+      String[] values = value.split(",");
+
+      for (String v : values) {
+	double   q     = 1.0;
+	double   w     = 0.0;
+	String[] parts = v.split(";");
+
+	Scriptable object = cx.newObject(accept);
+	object.put("valueOf", object, acceptValueOf);
+	object.put("value", object, parts[0].trim());
+
+	// Add all attributes
+	for (int i = 1; i < parts.length; ++i) {
+	  String[] attr = parts[i].split("=", 2);
+
+	  if (attr.length == 2) {
+	    // Parse Q factor
+	    if (attr[0].trim().equals("q")) {
+	      q = Double.parseDouble(attr[1].trim());
+	    }
+	    else {
+	      object.put(attr[0].trim(), object, attr[1].trim());
+	    }
+	  }
+	}
+
+	object.put("q", object, "" + q);
+
+	// Calculate implicit weight
+	if (parts[0].trim().equals("*/*")) {
+	  w = 0.0000;
+	}
+	else if (parts[0].trim().endsWith("/*")) {
+	  w = 0.0001;
+	}
+	else {
+	  w = 0.0002;
+	}
+
+	// Attributes give extra points
+	w += parts.length * 0.00001;
+
+
+	// Add to tree multi-map, inverse order
+	double key = -(q + w);
+
+	List<Scriptable> l = objects.get(key);
+
+	if (l == null) {
+	  l = new ArrayList<Scriptable>();
+	  objects.put(key, l);
+	}
+
+	l.add(object);
+      }
+
+      Scriptable object = cx.newArray(accept, objects.size());
+      accept.put(subname, accept, object);
+
+      int i = 0;
+      for (List<Scriptable> l : objects.values()) {
+	for (Scriptable s : l) {
+	  object.put(i++, object, s);
+	}
+      }
+    }
+
+
+    private void handleQueryHeader(String value) {
+      if (value.length() > 0) {
+	String[] args = value.split("&");
+
+	for (String arg : args) {
+	  String[] nv = arg.split("=", 2);
+
+	  try {
+	    String n = URLDecoder.decode(nv[0], "UTF-8").trim();
+
+	    if (nv.length == 1) {
+	      ScriptableObject.putProperty(query, makeXMLName(n), "");
+	    }
+	    else if (nv.length == 2) {
+	      String v = URLDecoder.decode(nv[1], "UTF-8");
+	      ScriptableObject.putProperty(query, makeXMLName(n), v);
+	    }
+	  }
+	  catch (UnsupportedEncodingException ex) {
+	    // Ignore illegal headers -- Or throw?
+	  }
+	}
+      }
+    }
+
+
+    public void handleContentHeader(String name, String value) {
+      if (name.startsWith("Content-")) {
+	if (name.equals("Content-Type")) {
+	  contentType = value;
+	}
+	else if (name.equals("Content-Length")) {
+	  contentLength = Long.parseLong(value);
+	}
+	else {
+	  throw new ESXXException(501, "Unsupported Content header: " + name);
+	}
+      }
+    }
+
+
+    private void parseMessage(Request request, Context cx, Scriptable scope) {
+      ESXX esxx = ESXX.getInstance();
+
+      // Consume SOAP message, if any
+      // TODO: Add a SOAP handler in Parser.java
+      if (soapAction != null) {
+	try {
+	  message = MessageFactory.newInstance(
+	      SOAPConstants.DYNAMIC_SOAP_PROTOCOL).createMessage(mimeHeaders,
+								 request.getInputStream());
+	}
+	catch (IOException ex) {
+	  throw new ESXXException("Unable to read SOAP message stream: " + ex.getMessage());
+	}
+	catch (SOAPException ex) {
+	  throw new ESXXException("Invalid SOAP message: " + ex.getMessage());
+	}
+	finally {
+	  try { request.getInputStream().close(); } catch (Exception ex) {}
+	}
+      }
+      else if (contentType != null && contentLength > 0) {
+	try {
+	  HashMap<String,String> params = new HashMap<String,String>();
+	  String                 ct     = ESXX.parseMIMEType(contentType, params);
+
+	  if ("application/x-www-form-urlencoded".equals(ct)) {
+	    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+	    IO.copyStream(request.getInputStream(), bos);
+	    handleQueryHeader(bos.toString("UTF-8"));
+	  }
+	  else {
+	    message = esxx.parseStream(ct, params, request.getInputStream(), 
+				       request.getScriptFilename().toURL(),
+				       null,
+				       new java.io.PrintWriter(request.getDebugWriter()),
+				       cx, scope);
+	  }
+	}
+	catch (Exception ex) {
+	  throw new ESXXException("Unable to parse request entity: " + ex.getMessage());
+	}
+	finally {
+	  try { request.getInputStream().close(); } catch (Exception ex) {}
+	}
+      }
+    }
+
+
+    private String makeXMLName(String s) {
+      char[] chars = s.toCharArray();
+
+      if(!isNameStartChar(chars[0])) {
+	chars[0] = '_';
+      }
+
+      for (int i = 1; i < chars.length; ++i) {
+	if (!isNameChar(chars[i])) {
+	  chars[i] = '_';
+	}
+      }
+
+      return new String(chars);
+    }
+
+    private static boolean isNameStartChar(char ch) {
+      return (Character.isLetter(ch) || ch == '_');
+    }
+
+    private static boolean isNameChar(char ch) {
+      return (isNameStartChar(ch) || Character.isDigit(ch) || ch == '.' || ch == '-');
+    }
+}
---
+++ tags/0.9.20080927/src/org/esxx/js/JSURI.java
@@ -0,0 +1,377 @@
+/*
+     ESXX - The friendly ECMAscript/XML Application Server
+     Copyright (C) 2007-2008 Martin Blom <martin@blom.org>
+
+     This program is free software: you can redistribute it and/or
+     modify it under the terms of the GNU General Public License
+     as published by the Free Software Foundation, either version 3
+     of the License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.esxx.js;
+
+import java.net.URI;
+import java.net.URL;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.lang.reflect.Constructor;
+import org.esxx.ESXX;
+import org.esxx.ESXXException;
+import org.esxx.js.protocol.ProtocolHandler;
+import org.mozilla.javascript.*;
+import org.mozilla.javascript.regexp.NativeRegExp;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class JSURI
+  extends ScriptableObject {
+  public JSURI() {
+    super();
+  }
+
+  public JSURI(URI uri) {
+    super();
+    this.uri = uri;
+
+    protocolHandler = getProtocolHandler();
+  }
+
+  @Override
+  public String toString() {
+    return uri.toString();
+  }
+
+  @Override
+    public String getClassName() {
+    return "URI";
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Object getDefaultValue(Class typeHint) {
+    return "[object URI: " + uri.toString() + "]";
+  }
+
+  static public Object jsConstructor(Context cx,
+				     java.lang.Object[] args,
+				     Function ctorObj,
+				     boolean inNewExpr)
+    throws java.net.URISyntaxException {
+    JSURI prop_src_uri = null;
+    URI uri = null;
+
+    if (args.length < 1 || args[0] == Context.getUndefinedValue()) {
+      throw Context.reportRuntimeError("Missing argument");
+    }
+    else if (args.length < 2 || args[1] == Context.getUndefinedValue()) {
+      if (args[0] instanceof JSURI) {
+	prop_src_uri = (JSURI) args[0];
+	uri = prop_src_uri.uri;
+      }
+      else if (args[0] instanceof URL) {
+	uri = ((URL) args[0]).toURI();
+      }
+      else {
+	JSESXX js_esxx = JSGlobal.getJSESXX(cx, ctorObj);
+
+	if (js_esxx != null) {
+	  JSURI location = js_esxx.jsGet_wd();
+
+	  if (location != null) {
+	    uri = location.uri.resolve(Context.toString(args[0]));
+	  }
+	}
+
+	if (uri == null) {
+	  uri = new URI(Context.toString(args[0]));
+	}
+      }
+    }
+    else if (args.length >= 2) {
+      try {
+	prop_src_uri = (JSURI) args[0];
+	uri = prop_src_uri.uri.resolve(Context.toString(args[1]));
+      }
+      catch (ClassCastException ex) {
+	throw Context.reportRuntimeError("Double argument must be URI and String");
+      }
+    }
+
+    JSURI rc = new JSURI(uri);
+
+    if (prop_src_uri != null) {
+      // Copy local properties from previous JSURI object
+      for (Object o : prop_src_uri.getIds()) {
+	if (o instanceof String) {
+	  String key = (String) o;
+
+	  rc.put(key, rc, prop_src_uri.get(key, prop_src_uri));
+	}
+	else {
+	  int key = (Integer) o;
+
+	  rc.put(key, rc, prop_src_uri.get(key, prop_src_uri));
+	}
+      }
+    }
+
+    return rc;
+    //      return createJSURI(uri);
+  }
+
+  public static void finishInit(Scriptable scope, 
+				FunctionObject constructor,
+				Scriptable prototype) {
+    // Create and make these properties in the prototype visible
+    Context cx = Context.getCurrentContext();
+
+    Scriptable jars = cx.newArray(prototype, 0);
+    jars.put(0, jars, cx.newArray(jars, 0));
+
+    defineProperty(prototype, "params",  cx.newArray(prototype, 0), ScriptableObject.PERMANENT);
+    defineProperty(prototype, "auth",    cx.newArray(prototype, 0), ScriptableObject.PERMANENT);
+    defineProperty(prototype, "jars",    jars,                      ScriptableObject.PERMANENT);
+    defineProperty(prototype, "headers", cx.newArray(prototype, 0), ScriptableObject.PERMANENT);
+  }
+
+  public static Object jsFunction_load(Context cx, Scriptable thisObj,
+				       Object[] args, Function funObj)
+    throws Exception {
+    JSURI  js_this = checkInstance(thisObj);
+    String type    = null;
+    HashMap<String,String> params = new HashMap<String,String>();
+
+    if (args.length >= 1 && args[0] != Context.getUndefinedValue()) {
+      type = ESXX.parseMIMEType(Context.toString(args[0]), params);
+    }
+
+    return js_this.protocolHandler.load(cx, thisObj, type, params);
+  }
+
+  public static Object jsFunction_save(Context cx, Scriptable thisObj,
+				       Object[] args, Function funObj)
+    throws Exception {
+    JSURI  js_this = checkInstance(thisObj);
+    String type    = null;
+    HashMap<String,String> params = new HashMap<String,String>();
+
+    if (args.length < 1 || args[0] == Context.getUndefinedValue()) {
+      throw Context.reportRuntimeError("Missing save() argument");
+    }
+
+    if (args.length >= 2 && args[1] != Context.getUndefinedValue()) {
+      type = ESXX.parseMIMEType(Context.toString(args[1]), params);
+    }
+
+    return js_this.protocolHandler.save(cx, thisObj, args[0], type, params);
+  }
+
+  public static Object jsFunction_append(Context cx, Scriptable thisObj,
+					 Object[] args, Function funObj)
+    throws Exception {
+    JSURI  js_this = checkInstance(thisObj);
+    String type    = null;
+    HashMap<String,String> params = new HashMap<String,String>();
+
+    if (args.length < 1 || args[0] == Context.getUndefinedValue()) {
+      throw Context.reportRuntimeError("Missing append() argument");
+    }
+
+    if (args.length >= 2 && args[1] != Context.getUndefinedValue()) {
+      type = ESXX.parseMIMEType(Context.toString(args[1]), params);
+    }
+
+    return js_this.protocolHandler.append(cx, thisObj, args[0], type, params);
+  }
+
+  public static Object jsFunction_remove(Context cx, Scriptable thisObj,
+					 Object[] args, Function funObj)
+    throws Exception {
+    JSURI  js_this = checkInstance(thisObj);
+    String type    = null;
+    HashMap<String,String> params = new HashMap<String,String>();
+
+    if (args.length >= 1 && args[0] != Context.getUndefinedValue()) {
+      type = ESXX.parseMIMEType(Context.toString(args[0]), params);
+    }
+
+    return js_this.protocolHandler.remove(cx, thisObj, type, params);
+  }
+
+  public static Object jsFunction_query(Context cx, Scriptable thisObj,
+					Object[] args, Function funObj)
+    throws Exception {
+    JSURI  js_this = checkInstance(thisObj);
+
+    return js_this.protocolHandler.query(cx, thisObj, args);
+  }
+
+  public Properties getParams(Context cx, URI uri) {
+    final Properties props = new Properties();
+
+    enumerateProperty(cx, "params", new PropEnumerator() {
+	public void handleProperty(Scriptable p, int s) {
+	  props.setProperty(Context.toString(p.get("name", p)), 
+			    Context.toString(p.get("value", p)));
+	}
+      }, uri, "");
+
+    return props;
+  }
+
+  public interface PropEnumerator {
+    void handleProperty(Scriptable prop, int score);
+  }
+
+  public Scriptable getAuth(Context cx, URI uri, String realm) {
+    return getBestProperty(cx, "auth", uri, realm);
+  }
+
+  public Scriptable getCookieJar(Context cx, URI uri) {
+    return getBestProperty(cx, "jars", uri, "");
+  }
+
+  public void enumerateHeaders(Context cx, PropEnumerator pe, URI uri) {
+    enumerateProperty(cx, "headers", pe, uri, "");
+  }
+
+  private Scriptable getBestProperty(Context cx, String name, URI uri, String realm) {
+    final Scriptable[] res = { null };
+    final int[]      score = { -1 };
+
+    enumerateProperty(cx, name, new PropEnumerator() {
+	public void handleProperty(Scriptable p, int s) {
+	  if (s > score[0]) {
+	    res[0] = p;
+	  }
+	}
+      }, uri, realm);
+
+    return res[0];
+  }
+
+  private void enumerateProperty(Context cx, String name, PropEnumerator pe,
+				 URI uri, String realm) {
+    String  scheme = uri.getScheme();
+    String  host   = uri.getHost();
+    Integer port   = uri.getPort();
+    String  path   = uri.getPath();
+
+    Object p = ScriptableObject.getProperty(this, name);
+
+    if (p instanceof Scriptable) {
+      Scriptable params = (Scriptable) p;
+
+      for (Object key : params.getIds()) {
+	if (key instanceof Integer) {
+	  p = params.get((Integer) key, params);
+	}
+	else {
+	  p = params.get((String) key, params);
+	}
+
+	if (p instanceof Scriptable) {
+	  Scriptable param = (Scriptable) p;
+	      
+	  int score = 0;
+
+	  score += filterProperty(cx, param, "scheme", scheme) * 1;
+	  score += filterProperty(cx, param, "realm",  realm)  * 2;
+	  score += filterProperty(cx, param, "path",   path)   * 4;
+	  score += filterProperty(cx, param, "port",   port)   * 8;
+	  score += filterProperty(cx, param, "host",   host)   * 16;
+
+	  if (score >= 0) {
+	    pe.handleProperty(param, score);
+	  }
+	}
+      }
+    }
+  }
+
+  private int filterProperty(Context cx, Scriptable param, String key, Object value) {
+    Object rule = param.get(key, param);
+
+    if (rule == null || rule == Scriptable.NOT_FOUND) {
+      return 0;
+    }
+
+    if (rule instanceof Number && value instanceof Number) {
+      return ((Number) rule).doubleValue() == ((Number) value).doubleValue() ? 1 : -1000;
+    }
+    else if (rule instanceof NativeRegExp) {
+      return ((NativeRegExp) rule).call(cx, this, (NativeRegExp) rule,
+					new Object[] { value }) != null ? 1 : -1000;
+    }
+    else {
+      return Context.toString(rule).equals(value.toString()) ? 1 : -1000;
+    }
+  }
+
+  private ProtocolHandler getProtocolHandler() {
+    String key     = uri.getScheme();
+    String handler = "org.esxx.js.protocol." + uri.getScheme().toUpperCase() + "Handler";
+
+    ProtocolHandler res = getProtocolHandler(key, handler);
+
+    if (res == null) {
+      try {
+	java.net.URL url = uri.toURL(); // Throws if the is no protocol handler for this URL
+	res = getProtocolHandler(key, "org.esxx.js.protocol.URLHandler");
+      }
+      catch (java.net.MalformedURLException ex) {}
+    }
+    
+    if (res == null) {
+      res = getProtocolHandler(key, "org.esxx.js.protocol.ProtocolHandler");
+    }
+
+    if (res == null) {
+      throw new IllegalStateException("Unable to create a ProtocolHandler for URI " + uri);
+    }
+    
+    return res;
+  }
+
+  private ProtocolHandler getProtocolHandler(String key, String handler) {
+    try {
+      Constructor<? extends ProtocolHandler> constr = schemeConstructors.get(key);
+
+      if (constr == null) {
+	Class<? extends ProtocolHandler> cls;
+	cls = Class.forName(handler).asSubclass(ProtocolHandler.class);
+	constr = cls.getConstructor(URI.class, JSURI.class);
+	schemeConstructors.put(key, constr);
+      }
+
+      return constr.newInstance(uri, this);
+    }
+    catch (Exception ex) {
+      return null;
+    }
+  }
+
+  protected static JSURI checkInstance(Scriptable obj) {
+    if (obj == null || !(obj instanceof JSURI)) {
+      throw Context.reportRuntimeError("Called on incompatible object");
+    }
+
+    return (JSURI) obj;
+  }
+
+  private static ConcurrentHashMap<String, Constructor<? extends ProtocolHandler>> schemeConstructors = new ConcurrentHashMap<String, Constructor<? extends ProtocolHandler>>();
+
+  private ProtocolHandler protocolHandler;
+  protected URI uri;
+
+  static final long serialVersionUID = -5445754832118781527L;
+}
